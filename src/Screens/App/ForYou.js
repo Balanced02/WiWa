38f5eb9465595d815b1f479ae2e066c2eec7de91
Daniel Adepoju <adepoju.daniel@stash.interswitch.com>
2019-09-27 20:16:00 +0100

    right arrow

--- src/Screens/App/ForYou.js
+++ src/Screens/App/ForYou.js
@@ -1,8 +1,10 @@
-import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, Image } from 'react-native';
 import fonts from '../../fonts';
 import colors from '../../colors';
-import {ScrollView} from 'react-native-gesture-handler';
+import { ScrollView } from 'react-native-gesture-handler';
+
+import ArrowRight from '../../../assets/images/arrow_right.png';
 
 import ProductPreview from '../../component/ProductPreview';
 
@@ -10,19 +12,25 @@ class ForYou extends Component {
   render() {
     return (
       <ScrollView style={{ paddingTop: 10 }}>
-        <View >
-          <Text style={{fontFamily: fonts.text, color: colors.theme, marginLeft: 10}}>
+        <View style={{ display: 'flex', flexDirection: 'row' }}>
+          <Text
+            style={{
+              fontFamily: fonts.text,
+              color: colors.theme,
+              marginLeft: 10,
+            }}>
             Recommended for you
           </Text>
-          <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} >
-            <ProductPreview />
-            <ProductPreview />
-            <ProductPreview />
-            <ProductPreview />
-            <ProductPreview />
-            <ProductPreview />
-          </ScrollView>
+          <Image source={ArrowRight} style={{ marginTop: 3 }} />
         </View>
+        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+          <ProductPreview />
+          <ProductPreview />
+          <ProductPreview />
+          <ProductPreview />
+          <ProductPreview />
+          <ProductPreview />
+        </ScrollView>
       </ScrollView>
     );
   }
